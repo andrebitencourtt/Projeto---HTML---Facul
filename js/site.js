a46e28611195913282d1eb79d309ff1e18e80912
bitencourt.andre@hotmail.com <andre_bitencourt@joinville.sc.senai.br>
2019-11-04 23:08:46 -0200

    [ Projeto ]

--- js/site.js
+++ js/site.js
@@ -1,14 +1,8 @@
-function validaContato(){
+/*function validaContato(){
 
     if(document.meuForm.nome.value==""){
         alert('Preencha o campo Nome.');
         document.meuForm.nome.focus();
-        return false;
-    }
-
-    if(document.meuForm.email.value==""){
-        alert('Preencha o campo E-mail.');
-        document.meuForm.email.focus();
         return false;
     }
 
@@ -18,6 +12,34 @@ function validaContato(){
         return false;
     }
 
+}*/
+
+function validaContato(){
+
+    var nome = document.meuForm.nome.value;
+	var expRegNome = new RegExp("^[A-zÁ-ü]{3,}([ ]{1}[A-zÁ-ü]{2,})+$");
+
+	if(!expRegNome.test(nome)){
+		alert("Preencha o campo Nome corretamente!");
+		document.meuForm.nome.focus();
+		return false;
+    }
+
+    if(document.meuForm.email.value==""){
+        alert('Preencha o campo E-mail.');
+        document.meuForm.email.focus();
+        return false;
+    }
+    
+    var fone = document.meuForm.telefone.value;
+	var expRegFone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+
+	if(!expRegFone.test(fone)){
+		alert("Preencha o campo Telefone corretamente!");
+		document.meuForm.telefone.focus();
+		return false;
+    }
+    
     if(document.meuForm.calendario.value==""){
         alert('Preencha o campo Calendário.');
         document.meuForm.calendario.focus();
@@ -39,6 +61,7 @@ function validaContato(){
         document.meuForm.mensagem.focus();
         return false;
     }
+
 }
 
 function createButtonEnviar(checkbox){
